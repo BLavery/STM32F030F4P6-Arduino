9fc54d14a474f01e06b94680b39d7277b025cbc3
Brian Lavery <BLavery@users.noreply.github.com>
2018-11-16 18:08:08 +1000

    Add files via upload

--- Arduino/libraries/miniSerial/miniSerial.cpp
+++ Arduino/libraries/miniSerial/miniSerial.cpp
@@ -2,16 +2,16 @@
 // A bit-banged low-footprint serial transmit/receive 
 // primarily for low-memory STM32F030F4P6    BL Nov 2018
 // By default uses PA9 as TX    PA10 as RX   -- regular 4-pin RX/TX end connector.
-// 8 bit no parity.  19200 seems ideal (default/max)  
+// 8 bit no parity.  19200 seems ideal (default)
 
 // tx will block during character transfer (abt 0.6 mSec each chr at 19200)
 // Your loop() must have free-running Serial.run(). No delay()s. Serial reception will suffer otherwise
 // reception is buffered
 // Transmit is not buffered, but does NOT stop reception processing.
 
-// Functions in this "Serial" are NOT LIKE ARDUINO regular Serial calls.
+// Functions in this "Serial" are an approximation to regular Serial calls.
 
-// V 0.2.0
+// V 0.5.0
 
 
 
@@ -39,38 +39,7 @@ void MiniSerial::begin(int baud, int tx, int rx)
 
 
 
-void MiniSerial::txStr(char * str, bool lf)             // TX a C string
-{
-	for (int i = 0; i<strlen(str); i++)
-		_putChar(str[i]);
-    if(lf) txStr("\n");
-
-}
-
-void MiniSerial::txByte(unsigned char ch, bool lf)             // TX a C byte
-{
-	_putChar(ch);
-    if(lf) txStr("\n");
-
-}
-
-void MiniSerial::txInt(int j, bool lf)                  // TX an int (as string)
-{
-	_txNum(j, 10, lf);
-}
-
-void MiniSerial::txBin(int j, bool lf)              
-{
-	_txNum(j, 2, lf);
-}
-
-void MiniSerial::txHex(int j, bool lf)              
-{
-	_txNum(j, 16, lf);
-}
-
-
-int MiniSerial::rxChar(void) // -1 =nothing/empty
+int MiniSerial::read(void) // -1 =nothing/empty
 {
 	// fetch from buffer
 	return rxBuf.dequeue();
@@ -86,17 +55,81 @@ void MiniSerial::run(void)  // MUST be called VERY frequently from your loop()
 	    rxBuf.enqueue((char) ch);
 }
 
-///////////////////////////////////////////////////////////////////
 
-void MiniSerial::_txNum(int j, int bits, bool lf)       // generic numeric formatting       
-{
-    char buffer[30];
-    itoa(j,buffer,bits); 
-    txStr(buffer);
-    if(lf) txStr("\n");
 
+
+void MiniSerial::print(char* str)
+{                                    
+	for (int i = 0; i<strlen(str); i++)
+		write(str[i]);
 }
 
+void MiniSerial::println(char* str)
+{                                      
+	print(str);
+	print("\n");
+}
+
+void MiniSerial::print(int j, int base)
+{                                      
+    char buffer[30];
+    itoa(j,buffer,base); 
+    print(buffer);
+}
+
+void MiniSerial::print(long j, int base)  // but for stm32, both int and long are 32bit!
+{                                       
+    print((int)j, base);
+}
+
+void MiniSerial::println(int j, int base)
+{                                       
+	print(j,base);
+	print("\n");
+}
+
+void MiniSerial::println(long j, int base)
+{                                       
+	print((int)j,base);  // long 32bit = int 32bit
+	print("\n");
+}
+
+void MiniSerial::print(double float_num, int prec){
+	                                       
+// precision - use 6 maximum
+	int d = float_num; // get the integer part
+	float f = float_num - d; // get the fractional part
+	if (d == 0 && f < 0.0){
+		write('-');
+		write('0');
+		f *= -1;
+	}
+	else if (d < 0 && f < 0.0){
+		print(d); 
+		f *= -1;
+	}
+	else{
+		print(d); 
+	}
+	// only when fractional part > 0, we show decimal point
+	if (f > 0.0){
+		write('.');
+		int f_shift = 1;
+		for (byte j=0; j<prec; j++){
+			f_shift *= 10;
+		}
+		print((int)(f*f_shift)); 
+	}
+}
+
+void MiniSerial::println(double float_num, int prec)
+{
+	print(float_num, prec);
+	print("\n");
+}
+
+
+/////////////////////////         HARDWARE IO:
 
 int MiniSerial::_getChar(void) // bit-bang rx
 // >=0 good rx char 
@@ -113,7 +146,6 @@ int MiniSerial::_getChar(void) // bit-bang rx
 		    rxState = READING;  
 		    rx_k = 0;
 		    return RXBUSY;
-			break;
 		
 		case  READING :  // 10 bit-length passes
 			if(micros()-rx_reftime < bitDelay)
@@ -123,7 +155,6 @@ int MiniSerial::_getChar(void) // bit-bang rx
 			if (rx_k > 9)
 				rxState = COMPLETE; 
 			return RXBUSY;
-			break;
 			
 		case COMPLETE :
 			rxState = RXIDLE; 
@@ -137,12 +168,12 @@ int MiniSerial::_getChar(void) // bit-bang rx
 }
 
 
-void MiniSerial::_putChar(unsigned char data)    // TX one byte bit-bang
+void MiniSerial::write(unsigned char data)    // TX one byte 
 {
 	if(pinTx<0) return;
 	
-  unsigned long starttime= micros();
   int chr = (data <<1) | 0b011000000000 ;
+  unsigned long starttime= micros();
 
   for (int i=11; i>0; i--)          // 1 start (0), 8 data bits, 2 stop (11)
   {
--- Arduino/libraries/miniSerial/miniSerial.h
+++ Arduino/libraries/miniSerial/miniSerial.h
@@ -5,34 +5,39 @@
 #include <Arduino.h>
 
 #define BITDELAY  52 
-//  104 9600  baud  52 19200    
+//  104 9600  baud  52 19200  
+//  not good above 19200
+
+#define HEX 16
+#define BIN 2
+#define OCT 8
 
 class MiniSerial
 {
   public:
 	MiniSerial(void);
 	void begin(int baudrate = 19200, int txpin = PA9, int rxpin = PA10);
-	void txByte(unsigned char data, bool lf=false);
-	void txStr(char * str, bool lf=false);
-	void txInt(int j, bool lf=false) ;
-	void txBin(int j, bool lf=false) ;
-	void txHex(int j, bool lf=false) ;
-	int  rxChar(void);
+	int  read(void);
 	void run(void);
+    void write(unsigned char data);
+    void print(double float_num, int prec=2);	// uses lots of flash space! Avoid?
+	void print(char* str);
+    void print(int, int = DEC);
+    void print(long, int = DEC);
+    void println(double float_num, int prec=2);	
+	void println(char* str = "");  // handles println() also
+    void println(int, int = DEC);
+    void println(long, int = DEC);
 
-
-	
-	
   private:
 
-  	void _putChar(unsigned char ch);
 	int  _getChar(void);
   	char _rx; // buffered chr
 	char _rx1 = 0;
 	int pinTx= -1;  // -1 = not begin'd
 	int pinRx ;
-	unsigned long bitDelay = 52;  // //  104 9600  baud  52 19200    26  38400 
-    void _txNum(int j, int bits, bool lf);
+	unsigned long bitDelay = 52;  // //  104 9600  baud  52 19200  
+
 	enum { RXIDLE, READING, COMPLETE } ;
 	enum { RXBUSY = -4, ERRNOTBEGIN = -3, ERRFRAME = -2,  RXNONE = -1 }; 
 	int rxState = RXIDLE;
