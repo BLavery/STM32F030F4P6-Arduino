7ec307848674ea153f1e78d04799a6e015ed57b9
Brian Lavery <BLavery@users.noreply.github.com>
2018-11-16 18:09:11 +1000

    Add files via upload

--- Arduino/libraries/miniSerial/examples/Tx-demo/Tx-demo.ino
+++ Arduino/libraries/miniSerial/examples/Tx-demo/Tx-demo.ino
@@ -8,10 +8,13 @@ void setup() {
 	// Serial.begin(19200, PA2, PA3);  // other option
 	delay(500);  // advise delay before serial output. Display wakeup reasons, not code reasons.
 #ifdef STM32F030x6  //this define is board ID
-    Serial.txStr("(STM32F030F4P6 board found)\n\n");
+    Serial.print("('030F4P6 found)\n\n");
     // original purpose of this library was for '030F4P6 board.  We found one!
 #endif
-	Serial.txStr("\nTX demo - software based, non native Serial port.\n\n");
+	Serial.print("\nminiSerial demo.\n");
+
+
+Serial.println();
 
 
 }
@@ -19,13 +22,14 @@ void setup() {
 
 
 void loop() {
-	//Demo of txByte, txInt, txHex, txBin, txStr
-	
-	Serial.txByte('H');  // no linefeed, so flows on
-	Serial.txStr("ello, I'm miniSerial...", true);// optional second parameter "true" adds linefeed
-	Serial.txInt(31741, true);
-	Serial.txStr("0x");   Serial.txHex(31741);
-	Serial.txStr(" 0b");  Serial.txBin(31741, true);
-	Serial.txStr("\n");
-	delay(2000);                     
+
+
+	Serial.write('H');  // no linefeed, so flows on
+	Serial.println("ello...");
+	Serial.print(31741);
+	Serial.print(" = 0x");   Serial.print(31741, HEX);
+	Serial.print(" = 0b");  Serial.print(31741, BIN);
+	Serial.println();
+    //Serial.println(-88.445, 2); // float is very expensive in flash (3.7k)
+	delay(2000);               
 }
